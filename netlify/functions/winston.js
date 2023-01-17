a50a13668bac0064f086d104ceebd09fd12ffe1f
Bryan Yunis <bbyunis@gmail.com>
2023-01-16 19:51:11 -0700

    add express boilerplate

---
+++ netlify/functions/winston.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const serverless = require("serverless-http")
+const app = express()
+const bodyParser = require("body-parser")
+const router = express.Router()
+
+app.use(bodyParser.json())
+app.use("/.netlify/functions/winston", router) // path must route to lambda
+app.use("/", router)
+
+router.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" })
+  res.write("<h1>Up and running</h1>")
+  res.end()
+})
+
+router.post("/mcdermott", async (req, res) => {
+  try {
+    // maybe do some database interaction or third-party API call here!
+    res.status(200).send({ data: "success" })
+  } catch (err) {
+    console.log(err)
+    res.status(400).send({ error: "bad request" })
+  }
+})
+
+module.exports = app
+module.exports.handler = serverless(app)
